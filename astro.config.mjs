9d37daa9f01641572c89cd27aafd97e14eafe38f
cilvia333 <cilvia333x@gmail.com>
2023-07-22 17:42:09 +0900

    chore: @astrojs/netlify を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig } from 'astro/config'
-import partytown from '@astrojs/partytown'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-    tailwind(),
-  ],
-})
+  integrations: [react(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), tailwind()],
+  output: "server",
+  adapter: netlify()
+});
