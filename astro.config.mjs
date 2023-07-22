2ac7d5eecf780c33a363617d1a7bcffe3214edad
cilvia333 <cilvia333x@gmail.com>
2023-07-22 17:42:26 +0900

    refactor:prettier かけた

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import partytown from '@astrojs/partytown'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), tailwind()],
-  output: "server",
-  adapter: netlify()
-});
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    tailwind(),
+  ],
+  output: 'server',
+  adapter: netlify(),
+})
